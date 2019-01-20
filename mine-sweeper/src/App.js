3dbd96adeaecc46311b0f7b0b824a4b2a1f41abb
Vivien Adnot <vivienadnot@amplement.com>
2019-01-20 13:53:06 +0100

    screen size limitation ok

--- mine-sweeper/src/App.js
+++ mine-sweeper/src/App.js
@@ -8,23 +8,54 @@ import Game from './Game/Game';
 import Rules from './Rules';
 import Drawer from './Drawer';
 
+const minimumScreenSizeOk = () => {
+
+    return window.innerWidth >= 1200 && window.innerHeight >= 700;
+
+}
+
+const renderApp = () => {
+
+    return (
+        <div className="root">
+            <DisplayRegisterDialogIfNeeded></DisplayRegisterDialogIfNeeded>
+            <DisplayLoginDialogIfNeeded></DisplayLoginDialogIfNeeded>
+            <AppBar />
+            <Drawer></Drawer>
+            <Switch>
+                <Route exact path='/' component={Rules}/>
+                <Route path='/rules' component={Rules}/>
+                <Route path='/game' component={Game}/>
+            </Switch>
+        </div>
+    );
+
+};
+
+const renderMinScreenSizeError = () => {
+
+    return (
+        <div className="root">
+            <AppBar />
+            <div style={{margin:"100px auto 0 auto"}}>
+                <h1 >Sorry, your screen is too small.</h1>
+                <h2>This game is designed for desktop screens.</h2>
+                <h2>The minimum required screen size to be able to play the game is 1200px x 700px.</h2>
+            </div>
+        </div>
+    );
+
+};
+
 class App extends Component {
 
     render() {
 
-        return (
-            <div className="root">
-                <DisplayRegisterDialogIfNeeded></DisplayRegisterDialogIfNeeded>
-                <DisplayLoginDialogIfNeeded></DisplayLoginDialogIfNeeded>
-                <AppBar />
-                <Drawer></Drawer>
-                <Switch>
-                    <Route exact path='/' component={Rules}/>
-                    <Route path='/rules' component={Rules}/>
-                    <Route path='/game' component={Game}/>
-                </Switch>
-            </div>
-        );
+        const ScreenToRender = (minimumScreenSizeOk())
+            ? renderApp
+            : renderMinScreenSizeError;
+
+        return <ScreenToRender/>
 
     }
 
